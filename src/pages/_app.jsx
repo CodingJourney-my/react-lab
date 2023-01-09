0d1c1e9901519f20dab2d7944c809546fae17425
Yuki Mizuno <muscular.mizuno@gmail.com>
2023-01-09 14:41:17 +0900

    ✨ Installed react-query.

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,13 +1,16 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Head from 'next/head'
 import 'src/styles/globals.css'
 
+const queryClient = new QueryClient()
+
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
-    </>
+    </QueryClientProvider>
   )
 }
